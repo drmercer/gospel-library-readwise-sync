3fa50774da8acefc0ac3430b4f890404b0579928
Dan Mercer <danielmercer1@gmail.com>
2024-02-21 22:51:54 -0700

    Add highlight-from-passage-and-offsets logic

--- src/popup.js
+++ src/popup.js
@@ -55,23 +55,58 @@ function getAllHighlightUris(annotations) {
     .map(h => h.uri);
 }
 
+// words can be split by spaces or dashes
+// TODO if any other word separators are discovered, add them here
+const SeparatorRegex = /(\s+|—|-)/g;
+
 function assembleHighlights(annotations, contents) {
   return annotations.map(a => {
     const highlights = a?.highlights ?? [];
     const contentObjs = highlights.map(h => contents[h.uri]);
     const locationUri = contentObjs[0] ? 'https://www.churchofjesuschrist.org/study' + contentObjs[0].referenceURI : undefined;
-    const contentsText = contentObjs
+    const mdParts = contentObjs
       .flatMap(c => c.content)
       .map(c => c.markup)
-      .map(htmlToMarkdown)
+      .map(htmlToMarkdown);
+    const fullMd = mdParts
       .join('\n\n');
     const highlightStart = highlights[0]?.startOffset;
     const highlightEnd = highlights[highlights.length - 1]?.endOffset;
+    const highlightMd = mdParts.map((part, i) => {
+      const h = highlights[i];
+      // highlights are stored as word offsets in the passage (or -1 for the start or end)
+      // TODO the offsets count footnotes as words... but we filter out the footnotes before this point... how to fix
+      const startOffset = Math.max(h.startOffset - 1, 0); // h.startOffset is 1-indexed
+      const endOffset = h.endOffset == -1 ? Number.POSITIVE_INFINITY : h.endOffset;
+      // split by words (keeping the separators because of the capturing group)
+      const parts = part.split(SeparatorRegex);
+      let offset = 0;
+      let j = 0;
+      while (offset < startOffset) {
+        const p = parts[j++];
+        if (p.match(SeparatorRegex)?.[0] !== p) {
+          // only count it if it's not a separator
+          offset++;
+        }
+      }
+      const startIndex = j;
+      while (offset < endOffset && j < parts.length) {
+        const p = parts[j++];
+        if (p.match(SeparatorRegex)?.[0] !== p) {
+          // only count it if it's not a separator
+          offset++;
+        }
+      }
+      const endIndex = j;
+      const highlightPart = parts.slice(startIndex, endIndex).join('').trim();
+      return highlightPart;
+    }).join('\n\n');
     return {
       locationUri,
-      contentsText,
       highlightStart,
       highlightEnd,
+      fullMd,
+      highlightMd,
     }
   })
 }
