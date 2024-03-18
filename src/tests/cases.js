fd3af79f91c4cf13ac99758236099ce361041bc8
Dan Mercer <danielmercer1@gmail.com>
2024-03-18 17:30:01 -0600

    Add more test cases

--- src/tests/cases.js
+++ src/tests/cases.js
@@ -40,4 +40,85 @@ export const testCases = [
       "noteMd": "Interesting to note how Jesus describes the objects of the wicked people's pride. Prideful people value things that aren't actually worth it."
     }
   ],
+  // Multiple verses
+  [
+    {
+      "locale": "eng",
+      "personId": "3504873342511069",
+      "contentVersion": 7,
+      "docId": "128351529",
+      "source": "Android Gospel Library | App: 6.6.2-(662000.1168416) | OS: 31",
+      "device": "android",
+      "created": "2023-10-20T04:42:21.769Z",
+      "annotationId": "cd428da9-26aa-4db4-b503-b07c7e64311a",
+      "folders": [],
+      "highlights": [
+        {
+          "uri": "/scriptures/bofm/alma/31.p24",
+          "pid": "128351555",
+          "color": "red",
+          "startOffset": 14,
+          "endOffset": -1
+        },
+        {
+          "uri": "/scriptures/bofm/alma/31.p25",
+          "pid": "128351556",
+          "color": "red",
+          "startOffset": -1,
+          "endOffset": -1
+        }
+      ],
+      "tags": [],
+      "type": "highlight",
+      "uri": "/scriptures/bofm/alma/31",
+      "lastUpdated": "2023-10-20T04:42:27.488Z"
+    },
+    {
+      "id": "cd428da9-26aa-4db4-b503-b07c7e64311a",
+      "source": {
+        "url": "https://www.churchofjesuschrist.org/study/scriptures/bofm/alma/31",
+        "author": "Book of Mormon",
+        "title": "Alma 31"
+      },
+      "fullMd": "24 Now when Alma saw this his heart was grieved; for he saw that they were a wicked and a perverse people; yea, he saw that their hearts were set upon gold, and upon silver, and upon all manner of fine goods.\n\n25 Yea, and he also saw that their hearts were lifted up unto great boasting, in their pride.",
+      "highlightMd": "he saw that they were a wicked and a perverse people; yea, he saw that their hearts were set upon gold, and upon silver, and upon all manner of fine goods.\n\n25 Yea, and he also saw that their hearts were lifted up unto great boasting, in their pride."
+    }
+  ],
+  // brackets in the source text
+  [
+    {
+      "locale": "eng",
+      "personId": "3504873342511069",
+      "contentVersion": 6,
+      "docId": "128364743",
+      "source": "Android Gospel Library | App: 4.1.0.8 | OS: 24",
+      "device": "android",
+      "created": "2017-06-02T01:43:49.788Z",
+      "annotationId": "07b28d43-292a-4ccc-b0ac-e82aff3c45f4",
+      "folders": [],
+      "highlights": [
+        {
+          "uri": "/scriptures/dc-testament/dc/131.p2",
+          "pid": "128364748",
+          "color": "green",
+          "startOffset": -1,
+          "endOffset": -1
+        }
+      ],
+      "tags": [],
+      "type": "highlight",
+      "uri": "/scriptures/dc-testament/dc/131",
+      "lastUpdated": "2017-06-02T01:43:49.788Z"
+    },
+    {
+      "id": "07b28d43-292a-4ccc-b0ac-e82aff3c45f4",
+      "source": {
+        "url": "https://www.churchofjesuschrist.org/study/scriptures/dc-testament/dc/131",
+        "author": "Doctrine and Covenants",
+        "title": "Doctrine and Covenants 131"
+      },
+      "fullMd": "2 And in order to obtain the highest, a man must enter into this order of the priesthood \\[meaning the new and everlasting covenant of marriage\\];",
+      "highlightMd": "2 And in order to obtain the highest, a man must enter into this order of the priesthood \\[meaning the new and everlasting covenant of marriage\\];"
+    }
+  ]
 ]
